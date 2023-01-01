da199a9d3f5b78287c9f9a7f9ebfc4321a43116a
i3cheese <i3cheese@yandex.ru>
2023-01-01 19:12:43 +0700

    chmod

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dots
+
+```sh
+sudo apt update
+sudo apt intall -y git curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/i3cheese/dots/master/first-init-linux.sh)"
+```
--- first-init-linux.sh
+++ first-init-linux.sh
@@ -1,10 +1,15 @@
-ssh-keygen -f ~/.ssh/github_id_rsa -N ""
+ssh-keygen -f ~/.ssh/github_id_rsa -N "" || exit 1
 
-cat ~/.ssh/github_id_rsa.pub
+cat ~/.ssh/github_id_rsa.pub || exit 1
 echo "add it to https://github.com/settings/keys\n"
 read ans
 
-git clone git@github.com:i3Cheese/dots.git
+echo "Host github.com
+  PreferredAuthentications publickey
+  IdentityFile ~/.ssh/github_id_rsa
+" > ~/.ssh/config || exit 1
+
+git clone git@github.com:i3Cheese/dots.git || exit 1
 
 ~/dots/build/tmux.sh
 ~/dots/build/zsh.sh
