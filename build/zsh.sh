0361483550f4bc4a58d5ee003f405ff0233efb3f
Сотников Даниил <i3Cheese@yandex.ru>
2024-01-18 23:30:09 +0300

    apt-get fzf

--- build/zsh.sh
+++ build/zsh.sh
@@ -3,4 +3,5 @@
 ln -s ~/dots/.zshrc ~/
 ln -s ~/dots/.zshenv ~/
 sudo apt-get install --yes zsh
+sudo apt-get install --yes fzf
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
