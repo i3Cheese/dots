31b0f9d9f5c8670d966a37616b51bd612f0ef574
Сотников Даниил <i3Cheese@yandex.ru>
2025-03-23 14:57:58 +0300

    Update zsh.sh

--- build/zsh.sh
+++ build/zsh.sh
@@ -5,3 +5,5 @@ ln -s ~/dots/.zshenv ~/ || exit 1
 sudo apt-get install --yes zsh || exit 1
 sudo apt-get install --yes fzf || exit 1
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || exit 1
+
+chsh -s /bin/zsh
